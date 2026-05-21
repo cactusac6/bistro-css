8c3d31737de505795dcd8409e7e17a52cc6b5e6e
cactusac6 <jankanka6@seznam.cz>
2026-05-21 21:20:20 +0200

    Update shipping.js

--- shipping.js
+++ shipping.js
@@ -2,17 +2,18 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 function initShipping(){
 
-const methods=document.querySelector("#order-shipping-methods");
-if(!methods)return;
+const wrap=document.querySelector("#order-shipping-methods");
+if(!wrap)return;
 
-// PSC BOX
+// vlozeni PSC
 if(!document.querySelector("#psc-box")){
 
-const first=document.querySelector(".shipping-billing-table");
+const title=wrap.querySelector("h4");
 
-if(first){
-first.insertAdjacentHTML("afterbegin",`
-<div id="psc-box" style="margin-bottom:15px">
+if(title){
+
+title.insertAdjacentHTML("afterend",`
+<div id="psc-box" style="margin:15px 0">
 <input id="psc" placeholder="Zadejte PSČ pro výpočet dopravy"
 style="
 width:100%;
@@ -26,11 +27,11 @@ box-sizing:border-box;
 ">
 </div>
 `);
-}
 
 }
 
-// ZONY
+}
+
 const zones={
 jaromer:["55101"],
 km5:["55102","55103","55203","55204"],
@@ -38,17 +39,14 @@ km10:["54901","55205","55224","55225","55104"],
 km15:["54701","54932","54941","54401","55221"]
 };
 
-// INPUT PSC
 const psc=document.querySelector("#psc")?.value.replace(/\s/g,"");
 
-// DOPRAVY
 const osobni=document.querySelector('input[value="63"]');
 const jaromer=document.querySelector('input[value="64"]');
 const km5=document.querySelector('input[value="65"]');
 const km10=document.querySelector('input[value="66"]');
 const km15=document.querySelector('input[value="67"]');
 
-// ZAMKNOUT ROZVOZY
 [jaromer,km5,km10,km15].forEach(el=>{
 
 if(!el)return;
@@ -64,7 +62,6 @@ row.style.opacity=".35";
 
 });
 
-// OSOBNI POVOLIT
 if(osobni){
 
 osobni.disabled=false;
@@ -80,10 +77,8 @@ osobni.checked=true;
 
 }
 
-// NIC NEZADANO
 if(!psc)return;
 
-// FUNKCE
 function enable(el){
 
 if(!el)return;
@@ -100,7 +95,6 @@ row.style.opacity="1";
 
 }
 
-// AUTO DOPRAVA
 if(zones.jaromer.includes(psc)) enable(jaromer);
 else if(zones.km5.includes(psc)) enable(km5);
 else if(zones.km10.includes(psc)) enable(km10);
